375904f882d8d89676d37000727edc3d4cd9abeb
soorajjsbabu <soorajjsbabu@gmail.com>
2020-07-27 19:51:23 +0530

    Created 404 page, fixed: display pages when searched,

--- Django/Project1/wiki/encyclopedia/urls.py
+++ Django/Project1/wiki/encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<slug:title>/", views.article, name="article")
 ]
--- Django/Project1/wiki/encyclopedia/views.py
+++ Django/Project1/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
 from . import util
 
 
@@ -8,3 +8,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def article(request, title):
+    print("Article triggered")
+    md_file = util.get_entry(title)
+    if not md_file:
+        return render(request, "encyclopedia/404.html")
+    return render(request, "encyclopedia/article.html", {
+        "the_text": md_file
+    })
