519a5f8e3ad7ec1595e01253e6cfbde60c4d7944
soorajjsbabu <soorajjsbabu@gmail.com>
2020-07-27 22:50:10 +0530

    Added random page and 'Create new page' feature

--- Django/Project1/wiki/encyclopedia/urls.py
+++ Django/Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("random/", views.random_page, name="random"),
+    path("new/", views.new, name="new"),
     path("<slug:title>/", views.article, name="article")
 ]
+    
--- Django/Project1/wiki/encyclopedia/views.py
+++ Django/Project1/wiki/encyclopedia/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from . import util
+import random
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    print("Index triggered")
+    print(request)
+    title = request.GET.get('q', None)
+    if title:
+        md_file = util.get_entry(title)
+        if not md_file:
+            return render(request, "encyclopedia/404.html")
+        return render(request, "encyclopedia/article.html", {
+            "the_text": md_file
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def article(request, title):
     print("Article triggered")
@@ -16,3 +28,13 @@ def article(request, title):
     return render(request, "encyclopedia/article.html", {
         "the_text": md_file
     })
+
+def random_page(request):
+    print("Random triggered")
+    entries = util.list_entries() 
+    selected_page = random.choice(entries)
+    return redirect('article', title=selected_page)
+
+def new(request):
+    return render(request, "encyclopedia/new.html")
+    
